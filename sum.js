7c4d8d527f0e0ec55e0a87c66aef3c2dfe97b153
Lisxt <alice14072005@gmail.com>
2025-05-28 11:14:44 -0300

    teste 1

---
+++ sum.js
@@ -0,0 +1,13 @@
+const sum = require('./sum.test');
+
+test('soma 1 + 2 para igualar 3', () => {
+expect(sum(1, 2)).toBe(3);
+});
+
+test('soma números negativos', () => {
+expect(sum(-1, -2)).toBe(-3);
+});
+
+test('soma 2 + 2 para igualar 4', () => {
+expect(sum(2, 2)).toBe(4);
+});
---
+++ sum.test.js
@@ -0,0 +1,6 @@
+test('Soma 2 + 2 para iagual a 4', () =>{
+    expect(soma(2,2).toBe(4));
+});
+test('Soma 2 + 3 para iagual a 5', () =>{
+    expect(soma(2,3).toBe(5));
+});
